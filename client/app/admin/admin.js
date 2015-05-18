994363d300f9a54fb5ee4e20cd5b43879848ac33
Markus Klinga <markus.klinga@sc5.io>
2015-05-18 14:54:11 +0300

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('critiqueApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
