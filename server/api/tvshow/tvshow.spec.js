a73ee78ceadcce0533d239674c532b2a21a8d293
Markus Klinga <markus.klinga@sc5.io>
2015-05-19 10:20:48 +0300

    Add unit tests for API

--- server/api/tvshow/tvshow.spec.js
+++ server/api/tvshow/tvshow.spec.js
@@ -3,11 +3,14 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var server = request.agent('http://localhost:9000');
 
 describe('GET /api/tvshows', function() {
 
+  var token;
+
   it('should fail when not logged in', function(done) {
-    request(app)
+    server
       .get('/api/tvshows')
       .expect(401)
       .expect('Content-Type', /text/)
@@ -17,4 +20,29 @@ describe('GET /api/tvshows', function() {
         done();
       });
   });
+
+  it('should allow authentication', function(done) {
+    server
+      .post('/auth/local')
+      .send({ email: 'test@test.com', password: 'test'})
+      .expect(200)
+      .end(function(err, res) {
+        should.exist(res.body.token);
+        token = res.body.token;
+        return done();
+      })
+  })
+
+  it('should fetch tvshows when authenticated', function(done) {
+    server
+      .get('/api/tvshows')
+      .set('authorization', 'Bearer ' + token)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
+        done();
+      });
+  })
 });
